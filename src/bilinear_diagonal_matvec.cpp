af6120cd2ca326e57cf810dc168c8b7ec50b170b
Jeremiah Giordani <jg0037@princeton.edu>
2025-07-20 13:57:08 -0400

    vectorized dot product, resulting in 6x speed over torch for 90% sparsity

--- src/bilinear_diagonal_matvec.cpp
+++ src/bilinear_diagonal_matvec.cpp
@@ -1,7 +1,7 @@
-// src/bilinear_diagonal_matvec.cpp
 #include "bilinear_diagonal_matvec.hpp"
 #include <cstring>     // std::memcpy
 #include <algorithm>   // std::fill
+#include <immintrin.h> // AVX2 intrinsics
 #include <omp.h>
 
 void quasi_dense_matvec_mt(
@@ -24,15 +24,34 @@ void quasi_dense_matvec_mt(
     // Determine thread count
     const int num_threads = (threads > 0 ? threads : omp_get_max_threads());
 
-    // Row‑wise dot of packed Wd and Xt
+    // Row‑wise vectorized dot of packed Wd and Xt
     #pragma omp parallel for num_threads(num_threads)
     for (uint32_t i = 0; i < m; ++i) {
-        const float* wrow = Q.Wd.data()  + size_t(i) * r;
-        const float* xrow = X.Xt.data()  + size_t(i) * r;
-        float acc = 0.0f;
-        for (uint32_t j = 0; j < r; ++j) {
+        const float* wrow = Q.Wd.data() + size_t(i) * r;
+        const float* xrow = X.Xt.data() + size_t(i) * r;
+        uint32_t j = 0;
+        __m256 acc_vec = _mm256_setzero_ps();
+
+        // Vectorized loop, 8 floats per iteration
+        for (; j + 8 <= r; j += 8) {
+            __m256 w = _mm256_loadu_ps(wrow + j);
+            __m256 x = _mm256_loadu_ps(xrow + j);
+            acc_vec = _mm256_fmadd_ps(w, x, acc_vec);
+        }
+
+        // Horizontal add of acc_vec
+        __m128 hi = _mm256_extractf128_ps(acc_vec, 1);
+        __m128 lo = _mm256_castps256_ps128(acc_vec);
+        __m128 sum128 = _mm_add_ps(lo, hi);
+        sum128 = _mm_hadd_ps(sum128, sum128);
+        sum128 = _mm_hadd_ps(sum128, sum128);
+        float acc = _mm_cvtss_f32(sum128);
+
+        // Remainder scalar cleanup
+        for (; j < r; ++j) {
             acc += wrow[j] * xrow[j];
         }
+
         y[i] += acc;
     }
 }
