997a4acbc563081be73ce03ca6d988468caefa8b
Jeremiah Giordani <jg0037@princeton.edu>
2025-07-26 12:30:55 -0400

    deleting old content

--- old/include/layer.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vector>
-
-class Layer {
-public:
-    virtual std::vector<float> forward(const std::vector<float>& input) const = 0;
-    virtual ~Layer() = default;
-};
--- old/tests/test_export.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-print("LOADING data/layer1.npz")
-x = np.load("data/layer1.npz")
-print("bias.shape:", x["bias"].shape)
-print("indptr.shape:", x["indptr"].shape)
